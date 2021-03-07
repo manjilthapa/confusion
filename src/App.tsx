78574a15e52c088ea929953d2ec5c5f2ba472414
manjilthapa <manjilthapa@gmail.com>
2021-03-08 00:14:45 +0100

    Components Part 2: used list of cards and onclick event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import MenuComponent from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <MenuComponent />
-    </div>
-  );
+export type Dish = {
+  id: number;
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+};
+
+type AppState = {
+  dishes: Dish[];
+};
+class App extends Component<{}, AppState> {
+  state: AppState = {
+    dishes: DISHES,
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
